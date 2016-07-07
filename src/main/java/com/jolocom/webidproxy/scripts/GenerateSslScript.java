b759a984315947f5e99c9a54590c73d13c00f247
Fred Grosskopf <mail@fredgrosskopf.com>
2016-07-07 13:28:53 +0200

    Updates call to generate-nginx-ssl-for-webid

--- src/main/java/com/jolocom/webidproxy/scripts/GenerateSslScript.java
+++ src/main/java/com/jolocom/webidproxy/scripts/GenerateSslScript.java
@@ -20,12 +20,11 @@ public class GenerateSslScript {
 
 	public static void execute(String username) throws IOException, InterruptedException {
 
-		final String domain = (Config.webidHost().indexOf(':') > 0) ? Config.webidHost().substring(0,  Config.webidHost().indexOf(':')) : Config.webidHost();
+		final String webid = (username + "." + (Config.webidHost().indexOf(':') > 0) ? Config.webidHost().substring(0,  Config.webidHost().indexOf(':')) : Config.webidHost());
 
 		StringBuffer line = new StringBuffer();
 		line.append(SCRIPT_PATH);
-		line.append(" -u " + username);
-		line.append(" -d " + domain);
+		line.append(" -i " + webid);
 		line.append(" -c " + CERTBOT_PATH);
 		line.append(" -w " + WEBROOT_PATH);
 		line.append(" -q");
